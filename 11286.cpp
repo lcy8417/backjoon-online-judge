87c02bdf91c2612d5fe11e30ac84c01707842696
lcy8417 <77670414+lcy8417@users.noreply.github.com>
2021-04-03 00:44:36 +0900

    Add files via upload

---
+++ 11286.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Hip{
+	int x;
+	Hip(int a){
+		x = a;
+	}
+	bool operator<(const Hip &b) const{
+		if(abs(x) == abs(b.x)){
+			return x > b.x;
+		}else return abs(x) > abs(b.x);
+	}
+};
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int N, x;
+	cin >> N;
+	priority_queue <Hip> q;
+	for(int i = 1; i <= N; i++){
+		cin >> x;
+		if(x == 0 && q.empty()) cout << 0 << "\n";
+		else if(x == 0) {
+			cout << q.top().x << "\n";
+			q.pop();
+		}
+		else q.push(Hip(x));
+	}
+}
