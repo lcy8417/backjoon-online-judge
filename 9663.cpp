af1013f63105bab32d129a3a0ec9c76067088f42
lcy8417 <77670414+lcy8417@users.noreply.github.com>
2021-04-06 00:28:44 +0900

    Add files via upload

--- 9663.cpp
+++ 9663.cpp
@@ -1,37 +1,28 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-
-int cs[16];
-int cnt, n;
+int queen[16];
+int ac;
+int N;
 
 void DFS(int L){
-	if(L == n){
-		cnt++;
-		return;
-	}else{
-		for(int i = 1; i <= n; i++){
-			bool flag = true;
-			for(int j = 0; j < L; j++){
-				if(cs[j] == i || abs(L - j) == abs(cs[j] - i)){
-					flag = false;
-					break;
-				}
+	if(L == N) ac++;
+	else{
+		for(int i = 0; i < N; i++){
+			queen[L] = i;
+			bool impossible = false;
+			for(int j = 0 ; j < L; j++){
+				if(queen[j] == queen[L] || abs(j-L) == abs(queen[j]-queen[L]))
+					impossible = true;
 			}
-			if(flag) {
-				cs[L] = i;
-				DFS(L+1);
-			}
+			if(!impossible) DFS(L+1);
 		}
 	}
 }
 
-int main() {
+int main(){
 	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> n;
+	cin >> N;
 	DFS(0);
-	cout << cnt;
-	return 0;
+	cout << ac;
 }
