c2b21f241ffb62381fdea96db162766fb854928b
lcy8417 <77670414+lcy8417@users.noreply.github.com>
2021-04-10 15:31:58 +0900

    Add files via upload

---
+++ 15656.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int a[10001];
+vector <int> v(10001);
+bool visited[10001];
+
+void DFS(int L){
+	if(L == M){
+		for(int i = 0; i < M; i++) cout << v[i] << " ";
+		cout << "\n";
+	}else{
+		for(int i = 1; i <= N; i++){
+			v[L] = a[i];
+			DFS(L+1);
+		}
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin >> N >> M;
+	for(int i = 1; i <= N; i++) cin >> a[i];
+	sort(a+1, a+1+N);
+	DFS(0);
+}
