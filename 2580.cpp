96d5d5caae11040f9f8c8f5db74893df0b8f45eb
lcy8417 <77670414+lcy8417@users.noreply.github.com>
2021-04-12 21:47:18 +0900

    Add files via upload

--- 2580.cpp
+++ 2580.cpp
@@ -1,77 +1,81 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#define endl '\n'
 using namespace std;
 
-int sdoque[10][10];
-int ch[10];
+int map[10][10];
+vector<pair<int, int>> slotV;
 
-void square(int x, int y){ // 3 x 3 사각형에 숫자있으면 체크 
-	int row = x / 3 * 3;
-	int col = y / 3 * 3; 
-	for(int i = row; i <= row + 2; i++){
-		for(int j = col; j <= col + 2; j++)
-			ch[sdoque[i][j]] = 1;
-	}
-}
-
-void rowCol(int x, int y){  // 행렬의 수 체크 
-	for(int i = 0; i < 9; i++){
-		ch[sdoque[x][i]] = 1;
-		ch[sdoque[i][y]] = 1;
-	}
-}
-
-bool isNum(int x){ // 행이 다 찼을떄 깊이 내려감 
-	for(int i = 0; i < 9; i++) if(sdoque[x][i] == 0) return false; // 행중에 빈값이 있으면 false
+bool chkSq(int r, int c, int num)
+{
+	int pivR = r / 3;
+	int pivC = c / 3;
+	for (int i = pivR * 3; i < pivR * 3 + 3; ++i)
+	for (int j = pivC * 3; j < pivC * 3 + 3; ++j)
+	if (map[i][j] == num)
+		return false;
 	return true;
 }
 
-void DFS(int L){
-	if(L == 1) {
-		cout << "\n";
-		for(int i = 0; i < 9; i++){
-			for(int j = 0; j < 9; j++)
-				cout << sdoque[i][j] << " ";
-			cout << "\n";
-		}
-		exit(0);
-	}else{
-		for(int i = 0; i < 9; i++){
-			if(sdoque[L][i] == 0){
-				//cout << L << " " << i << endl;
-				square(L, i);
-				rowCol(L, i);
-				for(int j = 1; j <= 9; j++) {
-					if(ch[j] == 0) {
-						//cout << ch[j] << endl;
-						sdoque[L][i] = j;
-						ch[j] = 1;
-						if(isNum(L)) {
-						for(int j = 1; j <= 9; j++) ch[j] = 0;
-						DFS(L+1);
-					}else{
-						DFS(L);
-						sdoque[L][i] = 0;
-						ch[sdoque[L][i]] = 0;
-						}	
-					}
-				}
+bool chkC(int c, int num)
+{
+	for (int i = 0; i < 9; ++i)
+	if (map[i][c] == num)
+		return false;
+	return true;
+}
+
+bool chkR(int r, int num)
+{
+	for (int j = 0; j < 9; ++j)
+	if (map[r][j] == num)
+		return false;
+	return true;
+}
+
+void DFS(int idx)
+{
+	if (idx == slotV.size())
+	{
+		for (int i = 0; i < 9; ++i)
+		{
+			for (int j = 0; j < 9; ++j)
+			{
+				cout << map[i][j] << " ";
 			}
+			cout << endl;
+		}
+		exit(EXIT_SUCCESS);
+	}
+
+	for (int num = 1; num <= 9; ++num)
+	{
+		int curR = slotV[idx].first;
+		int curC = slotV[idx].second;
+		if (chkC(curC, num) && chkR(curR, num) && chkSq(curR, curC, num))
+		{
+			map[curR][curC] = num;
+			DFS(idx + 1);
+			map[curR][curC] = 0;
 		}
 	}
 }
 
-int main() {
-	ios_base::sync_with_stdio(false);
-		
-	for(int i = 0; i < 9; i++){
-		for(int j = 0; j < 9; j++){
-			cin >> sdoque[i][j];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	for (int i = 0; i < 9; ++i)
+	{
+		for (int j = 0; j < 9; ++j)
+		{
+			cin >> map[i][j];
+			if (map[i][j] == 0)
+				slotV.push_back({ i, j });
 		}
 	}
 	DFS(0);
-	
-	
-	
-	return 0;
 }
